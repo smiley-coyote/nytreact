d559d6b962ee3b476e9836ddbb66821eb8a71843
smiley-coyote <nick.a.alex@gmail.com>
2018-09-27 18:14:00 -0400

    basic navbar complete and form

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -2,6 +2,13 @@ import "./Form.css";
 import React from "react";
 
 const Form = props => (
+  <div className="row">
+  <div className="col-sm-12">
+    <br />
+    <div className="panel panel-primary">
+      <div className="panel-heading">
+        <h3 className="panel-title"><strong> Search Parameters</strong></h3>
+      </div>
  <form className="search">
   <div className="form-group">
   <label htmlFor="term">Search Term:</label>
@@ -14,14 +21,34 @@ const Form = props => (
         id="term"
       />
       <br />
+      <div className="form-group">
+                <label for="pwd">Number of Records to Retrieve:</label>
+                <select className="form-control" id="num-records-select">
+							<option value="1">1</option>
+							<option value="5" selected>5</option>
+							<option value="10">10</option>
+						</select>
+              </div>
+              <div className="form-group">
+                <label for="start-year">Start Year (Optional):</label>
+                <input type="text" className="form-control" id="start-year" />
+              </div>
+              <div className="form-group">
+                <label for="end-year">End Year (Optional):</label>
+                <input type="text" className="form-control" id="end-year" />
+              </div>
       <button
         onClick={props.handleFormSubmit}
         className="btn btn-primary"
       >
         Search
       </button>
+      <button type="button" class="btn btn-default" id="clear-all">Clear Results</button>
   </div>
  </form>
+ </div>
+ </div>
+ </div>
      
           )
           
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -3,51 +3,28 @@ import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = props => (
-  <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <Link className="navbar-brand" to="/">
-      Pupster
+  <ul className="nav nav-tabs bg-primary">
+  <li className="nav-item">
+    <Link
+      to="/"
+      className={
+        window.location.pathname === "/" ? "nav-link active" : "nav-link"
+      }
+    >
+      Home
     </Link>
-    <div>
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <Link
-            to="/"
-            className={
-              window.location.pathname === "/" || window.location.pathname === "/about"
-                ? "nav-link active"
-                : "nav-link"
-            }
-          >
-            About
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link
-            to="/discover"
-            className={
-              window.location.pathname === "/discover"
-                ? "nav-link active"
-                : "nav-link"
-            }
-          >
-            Discover
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link
-            to="/search"
-            className={
-              window.location.pathname === "/search"
-                ? "nav-link active"
-                : "nav-link"
-            }
-          >
-            Search
-          </Link>
-        </li>
-      </ul>
-    </div>
-  </nav>
+  </li>
+  <li className="nav-item">
+    <Link
+      to="/about"
+      className={
+        window.location.pathname === "/about" ? "nav-link active" : "nav-link"
+      }
+    >
+      Saved
+    </Link>
+  </li>
+</ul>
 );
 
 export default Navbar;
